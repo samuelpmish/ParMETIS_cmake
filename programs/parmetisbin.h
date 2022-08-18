c0ae8bbfe5ea1ca2d8376a1d85a59a4840d6c728
Sam Mish <samuelpmish@gmail.com>
2022-08-17 21:40:32 -0700

    initial commit

---
+++ programs/parmetisbin.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 1997, Regents of the University of Minnesota
+ *
+ * par_metis.h
+ *
+ * This file includes all necessary header files
+ *
+ * Started 8/27/94
+ * George
+ *
+ * $Id: parmetisbin.h,v 1.1 2003/07/21 17:50:23 karypis Exp $
+ */
+
+/*
+#define DEBUG			1
+#define DMALLOC			1
+*/
+
+#include "GKlib.h"
+#include "parmetis.h"
+
+#include "../include/gklib_defs.h"
+#include "../include/rename.h"
+#include "../include/defs.h"
+#include "../include/struct.h"
+#include "../include/macros.h"
+#include "../include/proto.h"
+#include "proto.h"
+
+#define MAXNCON         32
